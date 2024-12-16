b5af0f071c9dda65c81d089fba64ba634bbf8022
ndSpacehead <damh17@gmail.com>
2024-12-16 21:27:48 +0300

    feat: server starting and shutdowning

--- internal/app/app.go
+++ internal/app/app.go
@@ -2,7 +2,16 @@ package app
 
 import (
 	"context"
+	"errors"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
+	"golang.org/x/sync/errgroup"
+
+	"victorina/internal/server"
 	"victorina/internal/storage"
 )
 
@@ -18,5 +27,37 @@ func Run() error {
 	if err != nil {
 		return err
 	}
-	return repo.Ping(context.Background())
+	defer repo.Close(context.Background())
+	if err := repo.Ping(context.Background()); err != nil {
+		return err
+	}
+	srv, err := server.New(server.Config{
+		Port: uint16(c.port),
+		Repo: repo,
+	})
+	if err != nil {
+		return err
+	}
+	sCtx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+	g, gCtx := errgroup.WithContext(sCtx)
+	g.Go(func() error {
+		err := srv.Serve()
+		if errors.Is(err, http.ErrServerClosed) {
+			return nil
+		}
+		return err
+	})
+	g.Go(func() error {
+		<-gCtx.Done()
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+		defer cancel()
+		if err := srv.Close(ctx); err != nil {
+			if !errors.Is(err, http.ErrServerClosed) {
+				return err
+			}
+		}
+		return nil
+	})
+	return g.Wait()
 }
